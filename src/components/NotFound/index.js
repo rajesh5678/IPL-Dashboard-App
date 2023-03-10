da406310dce55e93c6415796f4c6fbc3ae90b6ee
Rajesh <rajeshkuarsun56@gmail.com>
2023-03-10 14:50:36 +0000

    finish

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="notFound-cont">
+    <h1 className="para-123">Page Not Found</h1>
+  </div>
+)
+
+export default NotFound
